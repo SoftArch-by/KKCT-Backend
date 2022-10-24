1c950eefc7c85fd8cee04f3efbf66d5163b9885b
Jirapat Kaewsongsang <60883386+keedjk7@users.noreply.github.com>
2022-10-24 18:46:48 +0700

    get transaction(EE)

--- src/main/java/com/example/demo/Enterpreneur/Enterpreneur.java
+++ src/main/java/com/example/demo/Enterpreneur/Enterpreneur.java
@@ -1,6 +1,31 @@
 package com.example.demo.Enterpreneur;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.Transaction;
+import com.example.demo.repositories.TransactionRepository;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
+@RestController
 public class Enterpreneur {
+    private final TransactionRepository transactionRepository;
 
+    
+    public Enterpreneur(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    
+
+    @PostMapping("/getTransaction")
+    public String getTransaction(Iterable<String> C_id){
+
+		String txt = "";
+        for (Transaction transaction : transactionRepository.findAllById(C_id)){
+			txt+=transaction;
+            System.out.println(transaction);
+		}
+        return txt;
+    }
 }

6c68902a9d562409a14abfdf02e7a1fa16eff20c
oathathat <69292322+oathathat@users.noreply.github.com>
2022-11-02 20:11:00 +0700

    Account

---
+++ src/main/java/com/example/demo/models/Customer.java
@@ -0,0 +1,54 @@
+package com.example.demo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class Customer {
+    @Id
+    private String id;
+    @Field
+    private String email;
+    @Field
+    private String password;
+    @Field
+    private String citizenID;
+
+    public Customer(String id, String email, String password, String citizenID) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.citizenID = citizenID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCitizenID() {
+        return citizenID;
+    }
+
+    public void setCitizenID(String citizenID) {
+        this.citizenID = citizenID;
+    }
+}
---
+++ src/main/java/com/example/demo/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositories;
+
+
+import com.example.demo.models.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface CustomerRepository extends MongoRepository<Customer,String> {
+
+}

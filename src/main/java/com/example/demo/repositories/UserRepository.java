095ac1053ec29719f4b33ead749c23fcae1b0d1b
Jirakan.Cop <jirakan.cop@gmail.com>
2022-11-07 03:38:27 +0700

    latest rewrite

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repositories;
 import com.example.demo.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
 }

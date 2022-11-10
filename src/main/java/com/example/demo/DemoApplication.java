c2d3105bf00b39a5639d0d334725026b351f68ed
oathathat <69292322+oathathat@users.noreply.github.com>
2022-11-10 15:20:59 +0700

    commit00

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class DemoApplication{
 
 	public static void main(String[] args){
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

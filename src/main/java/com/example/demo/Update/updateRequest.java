f1b717f82b043130b994984afa3f6ae4a7f45bda
chanasorn <chanasorn.c@codium.co>
2022-11-17 21:35:17 +0700

    internal 500 fix

--- src/main/java/com/example/demo/Update/updateRequest.java
+++ src/main/java/com/example/demo/Update/updateRequest.java
@@ -1,9 +1,14 @@
 package com.example.demo.Update;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
 public class updateRequest {
+    
     private String ID;
     private Double paid;
-    updateRequest(String ID,Double paid){
+    updateRequest(@JsonProperty("ID") String ID,@JsonProperty("paid") Double paid){
         this.ID =ID;
         this.paid = paid;
     }

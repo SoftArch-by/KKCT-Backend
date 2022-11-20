301e4a82c15ad45bf008fc33efa73b7e339bb143
Chayanin Liengjindathaworn <maimokemm@hotmail.com>
2022-11-20 21:27:51 +0700

    use inheritance in createTransaction

--- src/main/java/com/example/demo/Update/createTransactionCid.java
+++ src/main/java/com/example/demo/Update/createTransactionCid.java
@@ -2,12 +2,8 @@ package com.example.demo.Update;
 
 import java.sql.Date;
 
-public class createTransactionCid {
+public class createTransactionCid extends createTransaction{
     private String citizen_id;
-    private String email;
-    private String OrganizationName ;
-    private Double Money;
-    private Date DueDate;
     createTransactionCid (
         String citizen_id,
         String email,
@@ -25,40 +21,4 @@ public class createTransactionCid {
     public String getCitizen_id() {
         return this.citizen_id;
     }
-
-    public void setCitizen_id(String citizen_id) {
-        this.citizen_id = citizen_id;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOrganizationName() {
-        return this.OrganizationName;
-    }
-
-    public void setOrganizationName(String OrganizationName) {
-        this.OrganizationName = OrganizationName;
-    }
-
-    public Double getMoney() {
-        return this.Money;
-    }
-
-    public void setMoney(Double Money) {
-        this.Money = Money;
-    }
-    public Date getDueDate() {
-        return this.DueDate;
-    }
-
-    public void setDueDate(Date DueDate) {
-        this.DueDate = DueDate;
-    }
-
 }
--- src/main/java/com/example/demo/Update/createTransactionEmail.java
+++ src/main/java/com/example/demo/Update/createTransactionEmail.java
@@ -2,12 +2,8 @@ package com.example.demo.Update;
 
 import java.sql.Date;
 
-public class createTransactionEmail {
+public class createTransactionEmail extends createTransaction{
     private String c_email;
-    private String email;
-    private String OrganizationName ;
-    private Double Money;
-    private Date DueDate;
     createTransactionEmail (
         String c_email,
         String email,
@@ -25,40 +21,4 @@ public class createTransactionEmail {
     public String getC_email() {
         return this.c_email;
     }
-
-    public void setC_email(String c_email) {
-        this.c_email = c_email;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOrganizationName() {
-        return this.OrganizationName;
-    }
-
-    public void setOrganizationName(String OrganizationName) {
-        this.OrganizationName = OrganizationName;
-    }
-
-    public Double getMoney() {
-        return this.Money;
-    }
-
-    public void setMoney(Double Money) {
-        this.Money = Money;
-    }
-    public Date getDueDate() {
-        return this.DueDate;
-    }
-
-    public void setDueDate(Date DueDate) {
-        this.DueDate = DueDate;
-    }
-
 }

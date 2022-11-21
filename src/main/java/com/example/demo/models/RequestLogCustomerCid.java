7d8776a10521871c197269e8bf4e6f0292d5bf6a
Jirapat Kaewsongsang <60883386+keedjk7@users.noreply.github.com>
2022-11-21 22:07:40 +0700

    create requestLogCustomer for extend

--- src/main/java/com/example/demo/models/RequestLogCustomerCid.java
+++ src/main/java/com/example/demo/models/RequestLogCustomerCid.java
@@ -4,22 +4,12 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
-public class RequestLogCustomerCid {
-    @Id
-    private String id;
-    @Field
-    private String Entrepreneur_Email;
-    @Field
-    private String Entrepreneur_Name;
-    @Field
+public class RequestLogCustomerCid extends RequestLogCustomer {
+
     private String Request_Customer_Cid;
-    @Field
-    private Date requestDate;
 
     public RequestLogCustomerCid(String Entrepreneur_Email,String Entrepreneur_Name,String Request_Customer_Cid){
         this.Entrepreneur_Email = Entrepreneur_Email;
@@ -28,43 +18,12 @@ public class RequestLogCustomerCid {
         this.requestDate = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEntrepreneur_Email() {
-        return Entrepreneur_Email;
-    }
-
-    public void setEntrepreneur_Email(String Entrepreneur_Email) {
-        this.Entrepreneur_Email = Entrepreneur_Email;
-    }
-     
     public String getRequest_Customer_Cid() {
         return Request_Customer_Cid;
     }
 
-    public void setRequest_Customer_Cid(String Request_Customer_Cid) {
-        this.Request_Customer_Cid = Request_Customer_Cid;
-    } 
-
-    public Date getRequestDate() {
-        return requestDate;
+    public void setRequest_Customer_Cid(String request_Customer_Cid) {
+        Request_Customer_Cid = request_Customer_Cid;
     }
 
-    public void setRequestDate(Date requestDate) {
-        this.requestDate = requestDate;
-    }
-
-    public String getEntrepreneur_Name() {
-        return Entrepreneur_Name;
-    }
-
-    public void setEntrepreneur_Name(String Entrepreneur_Name) {
-        this.Entrepreneur_Name = Entrepreneur_Name;
-    }
 }
